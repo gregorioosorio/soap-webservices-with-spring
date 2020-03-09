6c75d32984b05c5fa82b4c7674c6fee6dd6e3295
Gregorio Osorio <gregorio.osorio@lyra.com>
2020-03-09 11:03:22 -0500

    initial commit

---
+++ src/main/java/com/godev/soapwebserviceswithspring/SoapWebservicesWithSpringApplication.java
@@ -0,0 +1,13 @@
+package com.godev.soapwebserviceswithspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebservicesWithSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebservicesWithSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/godev/soapwebserviceswithspring/SoapWebservicesWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.godev.soapwebserviceswithspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapWebservicesWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
