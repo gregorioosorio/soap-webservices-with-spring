0014a8d5787da0f23e6d8925c5c55aed0e5a6cbc
Gregorio Osorio <gregorio.osorio@lyra.com>
2020-03-30 14:12:55 -0500

    changes to support wss

--- src/main/java/com/godev/soapwebserviceswithspring/WebServiceConfig.java
+++ src/main/java/com/godev/soapwebserviceswithspring/WebServiceConfig.java
@@ -1,20 +1,28 @@
 package com.godev.soapwebserviceswithspring;
 
+import java.util.Collections;
+
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.ws.client.support.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.server.endpoint.interceptor.PayloadLoggingInterceptor;
+import org.springframework.ws.soap.security.xwss.XwsSecurityInterceptor;
+import org.springframework.ws.soap.security.xwss.callback.SimplePasswordValidationCallbackHandler;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
 import org.springframework.xml.xsd.XsdSchema;
 
+
 @EnableWs
 @Configuration
 public class WebServiceConfig extends WsConfigurerAdapter {
+	private static final String WS_SCHEMA_PATH = "godev_contract.xsd";
 	private static final String NAMESPACE_URI = "http://godev.com/soap/webservices/demo";
 
 	@Bean
@@ -37,6 +45,34 @@ public class WebServiceConfig extends WsConfigurerAdapter {
 
 	@Bean
 	public XsdSchema countriesSchema() {
-		return new SimpleXsdSchema(new ClassPathResource("godev_contract.xsd"));
+		return new SimpleXsdSchema(new ClassPathResource(WS_SCHEMA_PATH));
 	}
+
+	@Bean
+	PayloadLoggingInterceptor payloadLoggingInterceptor() {
+		return new PayloadLoggingInterceptor();
+	}
+
+	@Bean
+	PayloadValidatingInterceptor payloadValidatingInterceptor() {
+		final PayloadValidatingInterceptor payloadValidatingInterceptor = new PayloadValidatingInterceptor();
+		payloadValidatingInterceptor.setSchema(new ClassPathResource(WS_SCHEMA_PATH));
+		return payloadValidatingInterceptor;
+	}
+
+	@Bean
+	XwsSecurityInterceptor securityInterceptor() {
+		XwsSecurityInterceptor securityInterceptor = new XwsSecurityInterceptor();
+		securityInterceptor.setCallbackHandler(callbackHandler());
+		securityInterceptor.setPolicyConfiguration(new ClassPathResource("security_policy.xml"));
+		return securityInterceptor;
+	}
+
+	@Bean
+	SimplePasswordValidationCallbackHandler callbackHandler() {
+		SimplePasswordValidationCallbackHandler callbackHandler = new SimplePasswordValidationCallbackHandler();
+		callbackHandler.setUsersMap(Collections.singletonMap("admin", "pwd123"));
+		return callbackHandler;
+	}
+
 }
